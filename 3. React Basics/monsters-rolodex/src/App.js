cd5303f6901834a083a7b9c532dc9e4d78ec167b
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-12-03 07:23:57 +0530

    Add Class Component in code

--- 3. React Basics/monsters-rolodex/src/App.js
+++ 3. React Basics/monsters-rolodex/src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstien',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        }, 
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        },
+        {
+          name: 'Zombister',
+          id: 'asc4'
+        },
+      ]
+    };
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello. I'm Kiranpal Singh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      {
+        this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+          ))}
     </div>
   );
+  }
 }
 
 export default App;
