785caf1216a797764b1e94dcebe5ea2276542736
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-12-05 06:39:45 +0530

    Add component life cycle methods and components

--- 3. React Basics/monsters-rolodex/src/App.js
+++ 3. React Basics/monsters-rolodex/src/App.js
@@ -1,38 +1,32 @@
 import logo from './logo.svg';
 import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstien',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asc2'
-        }, 
-        {
-          name: 'Zombie',
-          id: 'asc3'
-        },
-        {
-          name: 'Zombister',
-          id: 'asc4'
-        },
-      ]
+      monsters: []
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
   render() {
   return (
     <div className="App">
-      {
+      <CardList name="Yihua">
+        {
         this.state.monsters.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
-          ))}
+          ))
+        }
+      </CardList>
     </div>
   );
   }
