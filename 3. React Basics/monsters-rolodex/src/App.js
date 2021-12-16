d9e1703716ec3920c9ba3eb5266d3cc9cc982f3f
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-12-16 09:17:33 +0530

    Adding files

--- 3. React Basics/monsters-rolodex/src/App.js
+++ 3. React Basics/monsters-rolodex/src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
       );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder = 'Search Monsters'
           handleChange= {this.handleChange}
