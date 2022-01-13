1896f3986d8d957e789de6a5d3abf90f86bb4bad
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-13 12:10:06 +0530

    Add Shop data and Collection preview component

--- 4. Master Project E-Commerce Project/crwn-clothing/src/pages/shop/shop.data.js
+++ 4. Master Project E-Commerce Project/crwn-clothing/src/pages/shop/shop.data.js
@@ -9,36 +9,36 @@ const SHOP_DATA = [
             {
                 id: 1,
                 name: 'Brown Brim',
-                // imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-                imageUrl: "/images/shop-img/hats/brown-brim.png",
+                imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+                // imageUrl: "/images/shop-img/hats/brown-brim.png",
                 price: 25
             },
             {
                 id: 2,
                 name: 'Blue Beanie',
-                // imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-                imageUrl: "/images/shop-img/hats/blue-beanie.png",
+                imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+                // imageUrl: "/images/shop-img/hats/blue-beanie.png",
                 price: 18
             },
             {
                 id: 3,
                 name: 'Brown Cowboy',
-                // imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-                imageUrl: "/images/shop-img/hats/brown-cowboy.png",
+                imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+                // imageUrl: "/images/shop-img/hats/brown-cowboy.png",
                 price: 35
             },
             {
                 id: 4,
                 name: 'Grey Brim',
-                // imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-                imageUrl: "/images/shop-img/hats/grey-brim.png",
+                imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+                // imageUrl: "/images/shop-img/hats/grey-brim.png",
                 price: 25
             },
             {
                 id: 5,
                 name: 'Green Beanie',
-                // imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-                imageUrl: "/images/shop-img/hats/green-beanie.png",
+                imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+                // imageUrl: "/images/shop-img/hats/green-beanie.png",
                 price: 18
             },
             {
